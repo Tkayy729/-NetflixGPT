7fa043800e352f297cd8916b6011068d6a501cb4
Emmanuel Koduah Tweneboah <laptop@Tkayys-MacBook-Pro.local>
2025-12-05 16:56:12 +0100

    toggle logic

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  const handleToggle = () => {
+    setIsSignIn(!isSignIn);
+  };
+
   return (
     <div className="relative">
       <Header />
@@ -16,16 +22,27 @@ const Login = () => {
       </div>
       {/* Centered form */}
       <form className="flex flex-col justify-center items-center w-1/4 absolute bg-black/80 px-11 pt-11 pb-40 rounded-lg top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        {!isSignIn && (
+          <input
+            placeholder="Fullname"
+            className="w-full py-2 px-2 m-2 rounded outline-none border border-white  bg-black/10 p-6  placeholder:text-gray-300 placeholder:text-sm placeholder:italic"
+          />
+        )}
         <input
           placeholder="Email Address"
-          className="w-full py-2 px-2 m-2 rounded outline-none border border-white  bg-black/10 p-6"
+          className="w-full py-2 px-2 m-2 rounded outline-none border border-white  bg-black/10 p-6
+          placeholder:text-gray-300 placeholder:text-sm placeholder:italic"
         />
         <input
           placeholder="Password"
-          className="w-full  py-2 px-2 m-2 rounded outline-none border border-white  bg-black/10 p-6"
+          className="w-full  py-2 px-2 m-2 rounded outline-none border border-white  bg-black/10 p-6  placeholder:text-gray-300 placeholder:text-sm placeholder:italic"
         />
-        <button className="text-white w-full  py-1 px-2 m-2 rounded cursor-pointer bg-red-600">
-          Sign In
+        <button
+          type="button"
+          onClick={handleToggle}
+          className="text-white w-full  py-1 px-2 m-2 rounded cursor-pointer bg-red-600"
+        >
+          {isSignIn ? "Sign In" : "Sign Up"}
         </button>
         <p className="text-gray-50">OR</p>
         <button className="text-white w-full  py-1 px-2 m-2 rounded cursor-pointer bg-red-600">
@@ -43,7 +60,10 @@ const Login = () => {
           </label>
         </div>
         <div>
-          <p className="text-white text-sm mt-2">New to Netflix? <span className="text-bold text-white ">Sign up now</span></p>
+          <p className="text-white text-sm mt-2">
+            New to Netflix?{" "}
+            <span className="text-bold text-white ">Sign up now</span>
+          </p>
         </div>
       </form>
     </div>
